91098a310529fae79b610005d055aa03b0782845
carl-xiao <xiaobowen@newrank.cn>
2021-10-24 13:57:17 +0800

    添加注解

--- src/main/java/com/xiao/annotation/DBRouter.java
+++ src/main/java/com/xiao/annotation/DBRouter.java
@@ -1,10 +1,15 @@
 package com.xiao.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * @description: 路由注解
  * @author：Carl-Xiao
  * @date: 2021/10/20
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface DBRouter {
     /**
      * 分库分表字段
--- src/main/java/com/xiao/annotation/DBRouterStrategy.java
+++ src/main/java/com/xiao/annotation/DBRouterStrategy.java
@@ -1,10 +1,15 @@
 package com.xiao.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * @description: 路由策略，分表标记
  * @author：Carl-Xiao
  * @date: 2021/10/20
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface DBRouterStrategy {
     boolean splitTable() default false;
 }
